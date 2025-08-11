984d2ae68b7b0645a61b1f8d9449c850e84d551f
Eunki <xofho6@gmail.com>
2025-08-12 07:48:15 +0900

    Initialized svelte app

--- web-postit/svelte.config.js
+++ web-postit/svelte.config.js
@@ -6,7 +6,20 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter({
+			pages: "build",
+			assets: "build",
+			fallback: null,
+			precompress: false,
+			manifest, "manifest.json",
+		}),
+		appDir: "app",
+	},
 };
 
 export default config;
