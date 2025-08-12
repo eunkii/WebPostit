e48cfbce948833f01957d3c13fbee0e4d3d0ff5a
Eunki <xofho6@gmail.com>
2025-08-12 16:28:28 +0900

    Remove card feature implemented

--- web-postyt/src/routes/+page.svelte
+++ web-postyt/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { draggable, droppable, type DragDropState } from '@thisux/sveltednd';
     import { flip } from 'svelte/animate';
+    import Modal from './CardModal.svelte'
 
     interface Card {
         id: string;
@@ -37,14 +38,21 @@
                 class="relative aspect-square rounded-md bg-white shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl"
                 animate:flip={ {duration:200} }
             >
-                <div
+                <button
                     use:draggable={ {container: index.toString(), dragData: card} } 
-                    class="h-full w-full cursor-grab transition-all duration-300 hover:scale-105"
+                    class="h-full w-full cursor-grab transition-all duration-300 hover:scale-105" 
+                    onclick={() => (showModal = true)}
                 >
                     <div class="flex h-full items-center justify-center">
                         <p class="text-4xl">{card.content}</p>
                     </div>
-                </div>
+                </button>
+                <button
+                    class="flex absolute -top-2 -right-2 w-6 h-6 items-center justify-center rounded-full bg-red-500 text-white m-1 transition-all duration-300 hover:scale-120 hover:shadow-xl"
+                    onclick={() => handleRemove(card.id)}
+                >
+                    x
+                </button>
             </div>
         {/each}
         <button
---
+++ web-postyt/src/routes/CardModal.svelte
@@ -0,0 +1,13 @@
+<script>
+    let { showModal = $bindable(), header, children } = $props();
+
+    let dialog = $state();
+
+    $effect(() => {
+        if (showModal) dialog.showModal();
+    });
+</script>
+
+<dialog>
+    yar
+</dialog>
