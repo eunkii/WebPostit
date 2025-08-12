a21217fb559a05726b5b03dfaedfed35956a8b9a
Eunki <xofho6@gmail.com>
2025-08-12 14:56:01 +0900

    DND setup

--- web-postyt/src/routes/+page.svelte
+++ web-postyt/src/routes/+page.svelte
@@ -1,2 +1,67 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { draggable, droppable, type DragDropState } from '@thisux/sveltednd';
+    import { flip } from 'svelte/animate';
+
+    interface Card {
+        id: string;
+        content: string;
+    }
+
+    let cards = $state<Card[]>([]);
+
+    function handleAdd() {
+        cards = [ ...cards, {id: String(cards.length+1), content: ''} ];
+    }
+
+    function handleRemove(id: string) {
+        cards = cards.filter(card => card.id !== id);
+    }
+
+    /* if selected container is not the same as container to drop to,
+        handleDrop extracts selected item from cards array, and reorder */
+    function handleDrop(state: DragDropState<Card>) {
+        const { draggedItem, sourceContainer, targetContainer } = state;
+
+        if (!targetContainer || sourceContainer === targetContainer) return;
+
+        cards = cards.filter( (card: Card) => card.id !== draggedItem.id );
+        cards.splice(parseInt(targetContainer), 0, draggedItem);
+    }
+</script>
+
+<div class="min-h-screen p-8">
+    <div class="grid grid-cols-7 gap-6">
+        {#each cards as card, index(card.id)}
+            <div
+                use:droppable={ {container: index.toString(), callbacks: { onDrop: handleDrop }} } 
+                class="relative aspect-square rounded-md bg-white shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl"
+                animate:flip={ {duration:200} }
+            >
+                <div
+                    use:draggable={ {container: index.toString(), dragData: card} } 
+                    class="h-full w-full cursor-grab transition-all duration-300 hover:scale-105"
+                >
+                    <div class="flex h-full items-center justify-center">
+                        <p class="text-4xl">{card.content}</p>
+                    </div>
+                </div>
+            </div>
+        {/each}
+        <button
+            class="relative aspect-square rounded-md shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl cursor-pointer"
+            onclick={handleAdd}
+        >
+            <div class="flex h-full items-center justify-center">
+                <p class="text-4xl">+</p>
+            </div>
+        </button>
+    </div>
+</div>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+
+    :global(html) {
+        background-color: theme(--color-gray-100);
+    }
+</style>
